32794f43112b1c85b2a8e51e654fb27ffa7face8
Jason Yun <jasonyun@Jasons-MacBook-76.local>
2022-03-23 08:07:58 +0900

    add recursive search for css

--- frontend/style/webpack.config.js
+++ frontend/style/webpack.config.js
@@ -4,17 +4,24 @@ const fs = require('fs');
 const projectRoot = path.resolve(__dirname, "./");
 const tailwind_dist = "./dist/tailwind.css"
 
+function findAllCssExtensions(dir) {
+  const extensions = [];
+  const files = fs.readdirSync(dir);
+  files.forEach(file => {
+    const filePath = `${dir}/${file}`;
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      extensions.push(...findAllCssExtensions(filePath));
+    } else if (file.endsWith('.css') || file.endsWith('.scss')) {
+      extensions.push(filePath);
+    }
+  });
+  return extensions;
+}
+
 try {
   if (fs.existsSync(tailwind_dist)) {
-    const entryMap = ["./dist/tailwind.css"];  // run tailwind builder before this
-
-    fs.readdirSync(projectRoot + "/css")
-        .filter(file => {
-            return file.match(/.*\.(scss|css)$/);
-        })
-        .forEach(f => {
-            entryMap.push("./css/" + f)
-        });
+    const entryMap = ["./dist/tailwind.css"].concat(findAllCssExtensions(projectRoot));  // run tailwind builder before this
 
     module.exports = {
       mode: "development",
